7f51abe8049883edb1d5d12c42c18780913d22b8
Stephen Sparks <sdsparks@mnu.edu>
2021-03-12 14:48:24 -0600

    added html code

--- pantry_app/Pages/Recipes.aspx
+++ pantry_app/Pages/Recipes.aspx
@@ -1,12 +1,17 @@
 ﻿<%@ Page Title="Recipes" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Recipes.aspx.cs" Inherits="pantry_app.Contact" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <h1>Recipes</h1>
     <div>
-        <div class="table">
-            <div class="row">
-                <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        <div class="row">
+            <div class="col-xs-6">
+                <asp:GridView ID="gvRecipes" runat="server"></asp:GridView>
                 <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
             </div>
+        </div>
+        <div class="col-xs-5">
+            <asp:DetailsView ID="DetailsView1" runat="server" Height="50px" Width="125px"></asp:DetailsView>
+            <asp:SqlDataSource ID="SqlDataSource2" runat="server"></asp:SqlDataSource>
         </div>
     </div>
 </asp:Content>
--- pantry_app/Pages/Recipes.aspx.designer.cs
+++ pantry_app/Pages/Recipes.aspx.designer.cs
@@ -15,13 +15,13 @@ namespace pantry_app
     {
 
         /// <summary>
-        /// GridView1 control.
+        /// gvRecipes control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.GridView gvRecipes;
 
         /// <summary>
         /// SqlDataSource1 control.
@@ -31,5 +31,23 @@ namespace pantry_app
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
+        /// DetailsView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DetailsView DetailsView1;
+
+        /// <summary>
+        /// SqlDataSource2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
     }
 }
