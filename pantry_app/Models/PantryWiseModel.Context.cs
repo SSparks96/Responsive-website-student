8381474a418f2a3da3fc50dbfce6107f70ed0e21
Stephen Sparks <sdsparks@mnu.edu>
2021-03-17 18:17:34 -0500

    Inventory table

--- pantry_app/Models/PantryWiseModel.Context.cs
+++ pantry_app/Models/PantryWiseModel.Context.cs
@@ -27,5 +27,6 @@ namespace pantry_app.Models
     
         public virtual DbSet<PantryWiseUser> PantryWiseUsers { get; set; }
         public virtual DbSet<Security> Securities { get; set; }
+        public virtual DbSet<Inventory> Inventories { get; set; }
     }
 }
